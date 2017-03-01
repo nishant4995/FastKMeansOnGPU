d140d73e35895eb9ab74ea0c85483034822db34d
nishant <nishantyv@gmail.com>
2017-03-01 23:40:29 +0530

    Algorithm implemented in c using flattened arrays

--- main.h
+++ main.h
@@ -1,82 +1,89 @@
-#include <bits/stdc++.h>
-#include <cmath>
-#include "sys/time.h"
-#include "omp.h"
-#include "float.h"
-#include "xmmintrin.h"
+#ifndef _MAIN_H
+#define _MAIN_H value
 
-#define SIMD_WIDTH 4
+// #ifdef MNIST
+	// #define NUM_POINTS 70000
+	// #define DIMENSION 784
+	// #define ROUNDED_DIMENSION 1024
+	// #define NUM_CLUSTER 10
+	// #define ROUNDED_CLUSTER 16
+	// #define DATA "mnist"
+	// #else
+	// #ifdef THREE_D
+	// #define NUM_POINTS 434874
+	// #define DIMENSION 3
+	// #define ROUNDED_DIMENSION 4
+	// #define NUM_CLUSTER 5
+	// #define ROUNDED_CLUSTER 8
+	// #define DATA "3d_pro_5"
+	// #else
+	// #ifdef CIFAR
+	// #define NUM_POINTS 6000
+	// #define DIMENSION 3072
+	// #define ROUNDED_DIMENSION 4096
+	// #define NUM_CLUSTER 10
+	// #define ROUNDED_CLUSTER 16
+	// #define DATA "cifar"
+	// #else
+	// #ifdef BIRCH1
+	// #define NUM_POINTS 100000
+	// #define DIMENSION 2
+	// #define ROUNDED_DIMENSION 2
+	// #define NUM_CLUSTER 100
+	// #define ROUNDED_CLUSTER 128
+	// #define DATA "birch1"
+	// #else
+	// #ifdef BIRCH2
+	// #define NUM_POINTS 100000
+	// #define DIMENSION 2
+	// #define ROUNDED_DIMENSION 2
+	// #define NUM_CLUSTER 100
+	// #define ROUNDED_CLUSTER 128
+	// #define DATA "birch2"
+	// #else
+	// #ifdef BIRCH3
+	// #define NUM_POINTS 100000
+	// #define DIMENSION 2
+	// #define ROUNDED_DIMENSION 2
+	// #define NUM_CLUSTER 100
+	// #define ROUNDED_CLUSTER 128
+	// #define DATA "birch3"
+	// #endif
+	// #endif
+	// #endif
+	// #endif
+	// #endif
+	// #endif
 
-#ifdef MNIST
-#define NUM_POINTS 70000
-#define DIMENSION 784
-#define ROUNDED_DIMENSION 1024
-#define NUM_CLUSTER 10
-#define ROUNDED_CLUSTER 16
-#define DATA "mnist"
-#else
-#ifdef THREE_D
-#define NUM_POINTS 434874
-#define DIMENSION 3
-#define ROUNDED_DIMENSION 4
-#define NUM_CLUSTER 5
-#define ROUNDED_CLUSTER 8
-#define DATA "3d_pro_5"
-#else
-#ifdef CIFAR
-#define NUM_POINTS 6000
-#define DIMENSION 3072
-#define ROUNDED_DIMENSION 4096
-#define NUM_CLUSTER 10
-#define ROUNDED_CLUSTER 16
-#define DATA "cifar"
-#else
-#ifdef BIRCH1
 #define NUM_POINTS 100000
 #define DIMENSION 2
 #define ROUNDED_DIMENSION 2
 #define NUM_CLUSTER 100
 #define ROUNDED_CLUSTER 128
 #define DATA "birch1"
-#else
-#ifdef BIRCH2
-#define NUM_POINTS 100000
-#define DIMENSION 2
-#define ROUNDED_DIMENSION 2
-#define NUM_CLUSTER 100
-#define ROUNDED_CLUSTER 128
-#define DATA "birch2"
-#else
-#ifdef BIRCH3
-#define NUM_POINTS 100000
-#define DIMENSION 2
-#define ROUNDED_DIMENSION 2
-#define NUM_CLUSTER 100
-#define ROUNDED_CLUSTER 128
-#define DATA "birch3"
-#endif
-#endif
-#endif
-#endif
-#endif
-#endif
 
-using namespace std;
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <unistd.h>
+#include "sys/time.h"
+#include "omp.h"
+#include "float.h"
+#include <helper_cuda.h>
+#include "cuda_profiler_api.h"
 
-typedef float Point __attribute__((vector_size(ROUNDED_DIMENSION*sizeof(float))));
-// typedef float Distances __attribute__((vector_size(ROUNDED_CLUSTER*sizeof(float))));
-
-
-static inline double distance(Point, Point);
-static inline int sample_from_distribution(vector<double> &, int, int, double);
-static inline int sample_from_distribution_2 (vector<double> &, int , int , double );
+static inline float sd(double* a, int n);
+static inline float mean(double* a, int n);
 static inline double get_time_diff(struct timeval, struct timeval);
 
-static inline string mean(double*,int);
-static inline string sd(double*,int);
-
-vector<Point> d2_sample(vector<Point> &, vector<Point> &, vector<int> &, int, int);
-vector<Point> d2_sample_2(vector<Point> &,vector<Point> &,vector<int> &, int, int,vector<double> &);
-Point d2_sample_3(vector<Point> &data,vector<Point> &centers,vector<int> &weights,int num_samples, int size);
-Point mean_heuristic(vector<Point> &);
-vector<Point> independent_sample(vector<Point> &, vector<Point> &, double);
+__global__ void kernelAddConstant(int *g_a, const int b);
+int correctResult(int *data, const int n, const int b);
+int main_check();
+__global__ void comp_dist(double* dev_data,double* dev_distances,double* dev_centers,int centerIter,int numPoints,int dev_dimension);
+int sample_from_distribution (double* probabilities, int startIndex, int endIndex, double prob);
+double distance(double* p1, double* p2);
+double* mean_heuristic(double* multiset,int multisetSize);
+double* d2_sample(double* data,double* centers,int numPts, int numSamples, int size);
+#endif
